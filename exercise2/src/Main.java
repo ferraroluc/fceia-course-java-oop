2f9dd671253f5fc11efde6b2b0ab7335dd6cc412
Lucas Ferraro <ferraro.luc@outlook.com>
2023-05-28 14:14:40 -0300

    ready exercise 2

--- exercise2/src/Main.java
+++ exercise2/src/Main.java
@@ -2,83 +2,88 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    PeopleRegistration rp = new PeopleRegistration();
+        // init scanner
+        Scanner s = new Scanner(System.in);
 
-    Scanner s = new Scanner(System.in);
+        // objects
+        PeopleRegistration rp = new PeopleRegistration();
 
-    int option = -1;
-    while (option != 0) {
-        System.out.println("\nMenú de opciones:");
-        System.out.println("1. Insertar Persona");
-        System.out.println("2. Insertar Docente");
-        System.out.println("3. Insertar Empresario");
-        System.out.println("4. Suprimir");
-        System.out.println("5. Modificar (deshabilitada)");
-        System.out.println("6. Imprimir");
-        System.out.println("0. Salir");
-        System.out.println("\nIngrese opción:");
-        option = s.nextInt();
+        // menu
+        int option = -1;
+        while (option != 0) {
+            System.out.println("\nMenú de opciones:");
+            System.out.println("1. Insertar Persona");
+            System.out.println("2. Insertar Docente");
+            System.out.println("3. Insertar Empresario");
+            System.out.println("4. Suprimir");
+            System.out.println("5. Modificar (deshabilitada)");
+            System.out.println("6. Imprimir");
+            System.out.println("0. Salir");
+            System.out.println("\nIngrese opción:");
+            option = s.nextInt();
 
-        switch (option) {
-            case 1:
-                s.nextLine();
+            switch (option) {
+                case 1:
+                    s.nextLine();
 
-                String nombre;
-                System.out.println("Ingrese nombre:");
-                nombre = s.nextLine();
+                    String name;
+                    System.out.println("Ingrese nombre:");
+                    name = s.nextLine();
 
-                Persona p = new Persona(nombre);
-                rp.insertarPersona(p);
-                break;
+                    Person p = new Person(name);
+                    rp.addPerson(p);
+                    break;
 
-            case 2:
-                s.nextLine();
+                case 2:
+                    s.nextLine();
 
-                String nombreDocente;
-                System.out.println("Ingrese nombre:");
-                nombreDocente = s.nextLine();
-                String materiaDocente;
-                System.out.println("Ingrese materia:");
-                materiaDocente = s.nextLine();
+                    String teacherName;
+                    System.out.println("Ingrese nombre:");
+                    teacherName = s.nextLine();
+                    String teacherSubject;
+                    System.out.println("Ingrese materia:");
+                    teacherSubject = s.nextLine();
 
-                Teacher d = new Teacher(nombreDocente, materiaDocente);
-                rp.insertarPersona(d);
-                break;
+                    Teacher d = new Teacher(teacherName, teacherSubject);
+                    rp.addPerson(d);
+                    break;
 
-            case 3:
-                s.nextLine();
+                case 3:
+                    s.nextLine();
 
-                String nombreEmpresario;
-                System.out.println("Ingrese nombre:");
-                nombreEmpresario = s.nextLine();
-                Double sueldoEmpresario;
-                System.out.println("Ingrese sueldo:");
-                sueldoEmpresario = s.nextDouble();
+                    String businessmanName;
+                    System.out.println("Ingrese nombre:");
+                    businessmanName = s.nextLine();
+                    Double businessmanSalary;
+                    System.out.println("Ingrese sueldo:");
+                    businessmanSalary = s.nextDouble();
 
-                Businessman e = new Businessman(nombreEmpresario, sueldoEmpresario);
-                rp.insertarPersona(e);
-                break;
+                    Businessman e = new Businessman(businessmanName, businessmanSalary);
+                    rp.addPerson(e);
+                    break;
 
-            case 4:
-                s.nextLine();
+                case 4:
+                    s.nextLine();
 
-                String nombreEliminar;
-                System.out.println("Ingrese nombre:");
-                nombreEliminar = s.nextLine();
+                    String nameToDelete;
+                    System.out.println("Ingrese nombre:");
+                    nameToDelete = s.nextLine();
 
-                rp.eliminarPersona(nombreEliminar);
-                break;
+                    rp.removePerson(nameToDelete);
+                    break;
 
-            case 5:
-                System.out.println("Esta opción no se incluye en el alcance del grupo");
-                break;
+                case 5:
+                    System.out.println("Esta opción no se incluye en el alcance del curso");
+                    break;
 
-            case 6:
-                System.out.println("\nImpresión del registro:");
-                rp.imprimirRegistro();
-                break;
+                case 6:
+                    System.out.println("\nImpresión del registro:");
+                    rp.printRegistry();
+                    break;
+            }
         }
-    }
 
+        // close scanner
+        s.close();
     }
 }
